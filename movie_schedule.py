12f432c4728cbf3949805a2f8a66ccda98fee9bb
Karthik Kota <karthik.kanuparthi@gmail.com>
2025-10-02 19:18:48 -0500

    Update movie_schedule.py final plart

--- movie_schedule.py
+++ movie_schedule.py
@@ -4,4 +4,10 @@ current_movies = {'Movie A': '11:00am',
 print("We're showing the following movies:")
 for key in current_movies:
   print(key)
-
+movie = input('What movie would you like the showtime for?\n')
+showtime = current_movies.get(movie)
+if (showtime == None):
+  print("Requested movie isn't playing")
+else:
+  print(movie, 'is playing at', showtime)
+  
